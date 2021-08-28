7d60f4499ec3b23a5b6595969dc0765185f13085
toshikingggg <study.king7777@gmail.com>
2021-08-28 11:38:51 +0900

    [fix]utilにModalを切り分けた

---
+++ src/utils/Modal.tsx
@@ -0,0 +1,47 @@
+type ModalProps = {
+  data: string;
+  showModal: boolean;
+  onClickYes: () => void;
+  onClickNo: () => void;
+};
+
+const Modal: React.FC<ModalProps> = ({ data, showModal, onClickYes, onClickNo }) => {
+  return (
+    <>
+      {showModal ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="relative p-6 flex-auto">
+                  <p className="my-4 text-blueGray-500 text-lg leading-relaxed">{data}</p>
+                </div>
+                <div className="flex items-center justify-between p-6 border-t border-solid border-blueGray-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={onClickNo}
+                  >
+                    いいえ
+                  </button>
+                  <button
+                    className="text-green-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={onClickYes}
+                  >
+                    はい
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </>
+  );
+};
+
+export default Modal;
