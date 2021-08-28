7d5398bbf34f2101132a2fe1622fe7eac85cff34
toshikingggg <study.king7777@gmail.com>
2021-08-28 11:21:47 +0900

    [fix]Minor correction

--- src/pages/reservation/[date].tsx
+++ src/pages/reservation/[date].tsx
@@ -1,7 +1,7 @@
 import Layout from '../../components/Layout';
 
 const Day: React.FC = () => {
-  return <Layout title="register group">日毎の予定確認画面</Layout>;
+  return <Layout title="reservation">日毎の予定確認画面</Layout>;
 };
 
 export default Day;
