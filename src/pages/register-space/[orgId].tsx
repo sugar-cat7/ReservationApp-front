a08abafadd74c82530839c802f83315c52e5ec47
toshikingggg <study.king7777@gmail.com>
2021-08-30 23:39:04 +0900

    [fix]register space

---
+++ src/pages/register-space/[orgId].tsx
@@ -0,0 +1,11 @@
+import RegisterSpace from '../../components/RegisterSpace';
+import Layout from '../../components/Layout';
+const RegisterSpaces: React.FC = () => {
+  return (
+    <Layout title="register-space">
+      <RegisterSpace />
+    </Layout>
+  );
+};
+
+export default RegisterSpaces;
