a58238bf25b2a99db011c20f0cade9581b63611e
toshikingggg <study.king7777@gmail.com>
2021-09-12 18:20:39 +0900

    [add]仮user画面

---
+++ src/hooks/useUser.ts
@@ -0,0 +1,18 @@
+import useSWR from 'swr';
+import api from '../utils/fetch';
+
+type Props = {
+  user: { id: number; name: string; kana: string };
+  isLoading: boolean;
+  isError: boolean;
+};
+
+export const useUser = (): Props => {
+  const { data, error } = useSWR(`/api/user/${sessionStorage.getItem('user_id')}`, api.get);
+  console.log(data);
+  return {
+    user: data,
+    isLoading: !error && !data,
+    isError: error,
+  };
+};
---
+++ src/pages/setting.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Layout from '../components/Templates/Layout';
+import Setting from '../components/Templates/Setting';
+import { useUser } from '../hooks/useUser';
+
+const Mysetting: React.FC = () => {
+  const { user, isLoading } = useUser();
+
+  if (isLoading) {
+    return <div>Loding</div>;
+  }
+  console.log(user);
+  return (
+    <Layout title="設定">
+      <Setting user={user} />
+    </Layout>
+  );
+};
+
+export default Mysetting;
