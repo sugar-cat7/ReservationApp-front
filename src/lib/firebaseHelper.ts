1220c42151424c419eee73237ca0323c3136b1be
toshikingggg <study.king7777@gmail.com>
2021-09-17 23:58:42 +0900

    [add]local emulator setting

---
+++ src/lib/firebaseHelper.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+import { getStorage, ref } from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+initializeApp(firebaseConfig);
+
+export const storage = getStorage();
+
+export const storageRef = ref;
