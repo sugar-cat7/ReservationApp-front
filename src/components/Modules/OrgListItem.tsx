b591894ef28ad65384b0d6ad059900a64658fc2f
Hayato Mitomi <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-09-28 20:17:39 +0900

    OrgListItemの大きさを揃える

--- src/components/Modules/OrgListItem.tsx
+++ src/components/Modules/OrgListItem.tsx
@@ -20,12 +20,14 @@ const OrgListItem: React.FC<Props> = ({ value, onClick }) => {
       className={`bg-white w-72 mx-auto rounded-xl  shadow-md overflow-hidden md:max-w-2xl  hover:bg-gray hover:shadow-lg hover:border-transparent mr-4 ml-4 mb-4`}
       onClick={onClick}
     >
-      <div className="md:flex">
+      <div>
         <div className="p-2">
           <div className="tracking-wide text-sm text-indigo-500 font-semibold">
             グループ: {value.name}
           </div>
-          <div className="tracking-wide text-xs text-gray-500 ">{value.description}</div>
+          <div className="tracking-wide text-xs text-gray-500 truncate">
+            {value.description ? value.description : '説明はありません'}
+          </div>
         </div>
       </div>
     </div>
